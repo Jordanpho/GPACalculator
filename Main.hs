a90fb1e95decd97cf690cd6a1564e0bd6cf03617
Jordanpho <80399841+Jordanpho@users.noreply.github.com>
2021-04-24 23:37:07 -0400

    Update Main.hs
    Added further instructions for users.

--- Main.hs
+++ Main.hs
@@ -12,7 +12,7 @@ Also, these can be set to something else if you really want, except the variable
 3.  Type in the following the command to calculate your GPA. Use the following variables for course levels as well as grades.
 -}
 
-cp = 0
+cp = 0     -- All of these are variables which are used for inputs.
 hon = 0.5
 ap = 1
 apPlus = 1.5
@@ -21,5 +21,14 @@ b = 3
 c = 2
 d = 1
 
-gpa :: (Real a, Fractional b) => [a] -> b
-gpa xs = 2 * (realToFrac (sum xs) / (fromIntegral $ length xs))
+{-
+
+You will also have to put in your courses in a list format.
+Ex. An AP course with an A, you would put in the terminal "gpa [a,ap]" or "gpa [ap,a]"
+
+To put in multiple grades, just make the list as long as you need for all of your courses.
+Ex. GPA of 5 random courses: "gpa [a,ap,b,cp,a,apPlus,c,hon,d,cp]"   where each two entries in a list represent the course level and grade for a course.
+-}
+
+gpa :: (Real a, Fractional b) => [a] -> b      -- Defines the function and types for each part of it.
+gpa xs = 2 * (realToFrac (sum xs) / (fromIntegral $ length xs))    --Defines what the function does 
