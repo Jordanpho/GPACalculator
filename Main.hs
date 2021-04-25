8b09b5dd7f297a5a089361d2f0d5975994849731
Jordanpho <80399841+Jordanpho@users.noreply.github.com>
2021-04-24 22:05:01 -0400

    Update Main.hs
    Mostly have the program complete. There is still a couple of errors.

--- Main.hs
+++ Main.hs
@@ -1,1 +1,33 @@
+{-
+Here you can calculate any GPA when using ghci!
 
+Steps to Complete:
+
+1. Open up a terminal you would like to use.
+2. In the terminal, type in "ghci" to open ghci, or open a different compiler.
+3. Since I have not made this work with I/O yet, set up each variable by typing in the following commands in your compiler.
+
+let = cp = 0
+let hon = 0.5
+let ap = 1
+let apPlus = 1.5
+let a = 4
+let b = 3
+let c = 2
+let d = 1
+
+Also, these can be set to something else if you really want, except the variables can't have start with a capital letter, and symbols like "+" don't work.
+
+4.  Type in the following the command to calculate your GPA.
+-}
+
+gpa :: (Num a) => [a]  -> a
+gpa xs = (sum' xs) / (length' xs)
+
+length' :: (Num a) => [a] -> a  
+length' [] = 0  
+length' (_:xs) = 1 + length' xs
+
+sum' :: (Num a) => [a] -> a  
+sum' [] = 0  
+sum' (x:xs) = x + sum' xs 
